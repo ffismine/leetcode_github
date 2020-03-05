731ffc9b5af890535d7a0381daa21c5e999a4bdc
ffismine <42602140+ffismine@users.noreply.github.com>
2020-03-05 15:44:53 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	readme.md

---
+++ tag分类_栈/225_用队列实现栈.py
@@ -0,0 +1,82 @@
+# -*- coding:utf-8 -*-
+# Author : Zhang Xie
+# Date : 2020/3/4 23:51
+
+"""
+使用队列实现栈的下列操作：
+
+push(x) -- 元素 x 入栈
+pop() -- 移除栈顶元素
+top() -- 获取栈顶元素
+empty() -- 返回栈是否为空
+注意:
+
+你只能使用队列的基本操作-- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的。
+你所使用的语言也许不支持队列。 你可以使用 list 或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+你可以假设所有操作都是有效的（例如, 对一个空的栈不会调用 pop 或者 top 操作）。
+"""
+
+
+'''
+思考：
+在出队过程做改进：
+这题画图还是很快的，假设栈和队列的出栈是一模一样的，那么在入栈时进行处理（push），而出栈就是一样的操作就行了。
+对于同样的pop顺序54321，加入6时，栈的pop顺序为654321，而队列为543216，因此这题本质上转换为了：在push过程中将543216转换为654321的过程，即除去最后一位（push进来的）,剩下的元素逆序。
+用python语法可以写为：
+
+        for i in range(0,q_length-1):
+            self.q.append(self.q.pop(0))
+'''
+
+
+
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q = []
+
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.q.append(x)
+        q_length = len(self.q)
+        for i in range(0,q_length-1):
+            self.q.append(self.q.pop(0))  #除去最后一位（push进来的）,剩下的元素逆序
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.q.pop(0)
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.q[0]
+
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return not bool(self.q)
+
+a = MyStack()
+
+a.q = [5,4,3,2,1]
+
+a.push(6)
+a.push(7)
+print(a.pop())
+print(a.pop())
+print(a.pop())
+print(a.pop())
+print(a.pop())
+
+
---
+++ 链表/206.py
@@ -0,0 +1,65 @@
+# -*- coding:utf-8 -*-
+# Author : Zhang Xie
+# Date : 2020/3/4 18:26
+
+
+"""
+反转一个单链表。
+
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+
+进阶:你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+"""
+
+
+'''
+思考：链表翻转问题经常遇到，重点在于具体过程先对哪个节点动刀子。以1→2→3为例，翻转为321：1指向2→1为尾结点；2指向3→2指向1；3为尾结点→3指向2
+如果直接令2指向1，那么3的地址就丢失了。因此先来个变量指向3，然后2指向1，然后继续遍历。
+'''
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode):
+        # 申请两个节点，pre和 cur，pre指向None
+        pre = None
+        cur = head
+        # 遍历链表，while循环里面的内容其实可以写成一行
+        # 这里只做演示，就不搞那么骚气的写法了
+        while cur:
+            # 记录当前节点的下一个节点
+            tmp = cur.next
+            # 然后将当前节点指向pre
+            cur.next = pre
+            # pre和cur节点都前进一位
+            pre = cur
+            cur = tmp
+        return pre
+
+class Solution1:
+    def reverseList(self, head: ListNode) :
+        if not head or not head.next:
+            return head
+        nextNode = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return nextNode
+
+
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(3)
+l1.next.next.next = ListNode(4)
+#l2 = Solution().reverseList(l1)
+#print(l2.val, l2.next.val, l2.next.next.val, l2.next.next.next.val)
+
+l2 = Solution1().reverseList(l1)
+print(l2.val, l2.next.val, l2.next.next.val, l2.next.next.next.val)
