78d5ff9c642f56fda7fe58f562c633de732ad89e
ffismine <42602140+ffismine@users.noreply.github.com>
2020-03-15 04:16:24 +0800

    0315

--- tag分类_链表/86_分隔链表.py
+++ tag分类_链表/86_分隔链表.py
@@ -16,6 +16,8 @@
 小于就插到第一个后面，大于就插到第二个后面
 以类推。最终可以将所有节点划到两个临时节点后面
 最终将第一个临时链表插入第二个链表头部
+
+以下两种，第二种更简单
 '''
 
 
@@ -28,45 +30,43 @@ class ListNode:
 
 class Solution:
     def partition(self, head: ListNode, x: int) -> ListNode:
-        dummy1 = ListNode(-1)
-        dummy2 = ListNode(-1)
-        p1 = dummy1
-        p2 = dummy2
+        left_head = ListNode(-1)
+        right_head = ListNode(-1)
+        left = left_head
+        right = right_head
         while head:
             if head.val < x:
-                p1.next = head
-                p1 = p1.next
+                left.next = head  # p1.next并没有对p1操作，因此dummy和p1地址不变。等于head相当于把p1内部存储了东西
+                left = left.next
             else:
-                p2.next = head
-                p2 = p2.next
+                right.next = head
+                right = right.next
             head = head.next
-        # print(listNodeToString(dummy1.next))
-        # print(listNodeToString(dummy2.next))
-        p1.next = dummy2.next
-        p2.next = None
-        return dummy1.next
+        left.next = right_head.next
+        right.next = None
+        return left_head.next
+
 
 class Solution1:
     def partition(self, head: ListNode, x: int) -> ListNode:
-        left_h = ListNode(None)
-        left = left_h
-        right_h = ListNode(None)
-        right = right_h
+        left_head = ListNode(-1)
+        left = left_head
+        right_head = ListNode(-1)
+        right = right_head
         while head:
             if head.val < x:
-                left_h.next = ListNode(head.val)
-                left_h = left_h.next
+                left.next = ListNode(head.val)
+                left = left.next
             elif head.val >= x:
-                right_h.next = ListNode(head.val)
-                right_h = right_h.next
+                right.next = ListNode(head.val)
+                right = right.next
             head = head.next
-            continue
-        left_h.next = right.next
-        return left.next
+        left.next = right_head.next
+        return left_head.next
 
 
 l1 = ListNode(1)
 l1.next = ListNode(5)
 l1.next.next = ListNode(3)
 l1.next.next.next = ListNode(2)
-l2 = Solution().partition(l1, 3)
+l2 = Solution1().partition(l1, 3)
