04d598bdfb38b92a5a825756b3e47aa0b1f0006b
ffismine <42602140+ffismine@users.noreply.github.com>
2020-03-20 18:59:26 +0800

    0320

--- tag分类_递归&回溯/90_子集2.py
+++ tag分类_递归&回溯/90_子集2.py
@@ -20,8 +20,35 @@
 """
 
 '''
+原始数组排序后去重，就等价于第一个子集问题了
+但是需要注意，不能有重复的元素，所以要多一个集合来判断是否已经有了。
+而python的set不可以存储unhashable元素，所以要多一个tuple用以每次判断
 '''
 
 class Solution:
     def subsetsWithDup(self, nums):
-        pass
+        ans = []
+        item =[]
+        res_set = set()
+        nums.sort()
+
+        def helper(i, item, ans, res_set):
+            if i == len(nums):
+                return
+            item.append(nums[i])
+            # 浅拷贝实现append
+            real = item.copy()
+            real_ = tuple(real)
+            if real_ not in res_set:
+                res_set.add(real_)
+                ans.append(real)
+            helper(i + 1, item, ans, res_set)
+            item.pop()
+            helper(i + 1, item, ans, res_set)
+
+        real = item.copy()
+        ans.append(real)
+        helper(0, item, ans, res_set)
+        return ans
+
+print(Solution().subsetsWithDup([4,4,4,1,2]))
