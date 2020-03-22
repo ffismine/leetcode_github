c4f7a017321fa751ec4f8390d4ccb2f7826d3b85
ffismine <42602140+ffismine@users.noreply.github.com>
2020-03-22 17:06:51 +0800

    0322

---
+++ tag分类_二叉树与图/TreeNode.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# Author : Zhang Xie
+# Date : 2020/3/22 17:05
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
