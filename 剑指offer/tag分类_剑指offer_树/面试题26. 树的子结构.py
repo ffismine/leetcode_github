418bc0233b3935e1bf888593b4515b00b0b9513b
ffismine <42602140+ffismine@users.noreply.github.com>
2020-04-12 09:13:24 +0800

    面试题26

--- 剑指offer/tag分类_剑指offer_树/面试题26. 树的子结构.py
+++ 剑指offer/tag分类_剑指offer_树/面试题26. 树的子结构.py
@@ -29,6 +29,11 @@ B是A的子结构， 即 A中有出现和B相同的结构和节点值。
 输入：A = [3,4,5,1,2], B = [4,1]
 输出：true
 """
+'''
+Solution是很多人提到的，但是解法2更加清晰易懂
+Solution1 是通过树的深度遍历来找当前树是否和B根节点一样，如果一样，再去判断其他的是否一样。
+个人推荐Solution1
+'''
 from tag_tree import TreeNode
 
 
@@ -55,14 +60,39 @@ class Solution:
             return False
 
 
+class Solution1(object):
+    def isSubStructure(self, A, B):
+        result = False
+        if A and B:  # A和B都不为空
+            if A.val == B.val:
+                result = self.isSubTree(A, B)  # 递归的判断他们各自左右节点的值是不是相同
+            if not result:
+                result = self.isSubStructure(A.left, B)  # 不相等则将树A的左子树与B进行比较
+            if not result:
+                result = self.isSubStructure(A.right, B)  # 不相等则将树A的右子树与B进行比较
+        return result
+
+    def isSubTree(self, root_A, root_B):
+        if not root_B:  # 如果B为空,说明前面的节点都能一一对应上了,所以B是A的子树
+            return True
+        if not root_A:  # 如果A为空,则说明B不是他的子树
+            return False
+        if root_A.val != root_B.val:  # 节点值不相等,说明也不是
+            return False
+        # 判断左右子树是否符合
+        return self.isSubTree(root_A.left, root_B.left) and self.isSubTree(root_A.right, root_B.right)
+
+
+
 # A = [3,4,5,1,2], B = [4,1]
 A = TreeNode(3)
 A.left = TreeNode(4)
 A.right = TreeNode(5)
-A.left.left = TreeNode(1)
+A.right.left = TreeNode(1)
 A.left.right = TreeNode(2)
 
-B = TreeNode(4)
+B = TreeNode(5)
 B.left = TreeNode(1)
 
 Solution().isSubStructure(A, B)
+Solution1().isSubStructure(A, B)
