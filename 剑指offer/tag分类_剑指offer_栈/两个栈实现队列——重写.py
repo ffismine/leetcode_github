42c88848147468be7b3c98e95a32d88e9d295a83
ffismine <ffismine>
2020-06-08 16:56:15 +0800

    增加重写一题

---
+++ 剑指offer/tag分类_剑指offer_栈/两个栈实现队列——重写.py
@@ -0,0 +1,24 @@
+
+
+# 还行，没啥难度
+
+class CQueue:
+    def __init__(self):
+        self.stack = []
+        self.temp_stack = []
+
+    def appendTail(self, value: int) -> None:
+        # 原 -> 临时
+        while self.stack:
+            self.temp_stack.append(self.stack[-1])
+            self.stack.pop()
+        self.stack.append(value)
+        while self.temp_stack:
+            self.stack.append(self.temp_stack[-1])
+            self.temp_stack.pop()
+
+    def deleteHead(self) -> int:
+        if not self.stack:
+            return -1
+        else:
+            return self.stack.pop()
