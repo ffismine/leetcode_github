aa190c830014211b8b3bb49de2effecf218128c4
xiezhang <438581918@qq.com>
2020-07-07 10:13:53 +0800

    增加堆heapq库的用法

---
+++ definition/tag_heapq.py
@@ -0,0 +1,8 @@
+import heapq
+# 在这里顺便复习python堆的用法
+# heappush(heap, x) 将x压入堆中
+# heappop(heap) 从堆中弹出最小的元素
+# heapify(heap) 让列表具备堆特征
+# heapreplace(heap, x) 弹出最小的元素，并将x压入堆中，返回弹出的元素
+# nlargest(n, iter) 返回iter中n个最大的元素
+# nsmallest(n, iter) 返回iter中n个最小的元素
