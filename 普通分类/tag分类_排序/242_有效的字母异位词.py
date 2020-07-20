7b40d1d71202b11c8aae9472aa99d9a9837f8677
xiezhang <438581918@qq.com>
2020-07-20 10:54:50 +0800

    0720增加有效的字母异位词

---
+++ 普通分类/tag分类_排序/242_有效的字母异位词.py
@@ -0,0 +1,41 @@
+"""
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+示例 1:
+输入: s = "anagram", t = "nagaram"
+输出: true
+
+示例 2:
+输入: s = "rat", t = "car"
+输出: false
+
+说明:
+你可以假设字符串只包含小写字母。
+
+进阶:
+如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+
+"""
+
+"""
+思考：第一反应使用哈希表
+不过用排序也可以
+python可以一行解决 ：
+        return collections.Counter(s) == collections.Counter(t)
+"""
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        l1 = list(s)
+        l1.sort()
+        l2 = list(t)
+        l2.sort()
+        return l1 == l2
+
+        # return sorted(s) == sorted(t)
+
+        # return collections.Counter(s) == collections.Counter(t)
+
+
+print(Solution().isAnagram("a", "b"))
