b46c1105738504908ed629bd44523320a12826d3
xiezhang <438581918@qq.com>
2020-07-24 11:40:50 +0800

    0724增加最长回文子串

--- 普通分类/tag分类_动态规划/5_最长回文子串.py
+++ 普通分类/tag分类_动态规划/5_最长回文子串.py
@@ -11,7 +11,49 @@
 输出: "bb"
 """
 
+"""
+动态规划： dp[i][j] = dp[i+1][j-1] and (s[i] == s[j])
+考虑边界条件： j-1 <= i+1 即 
+i < j <= i+2
+在此边界条件下，相当于i和j之间的数为1个或者0个
+因此只需要验证dp[i][j] = s[i] == s[j]
+
+否则 dp[i][j] = dp[i+1][j-1] and (s[i] == s[j])
+
+"""
+
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        len_ = len(s)
+        if len_ <= 1:
+            return s
+
+        dp = [[False for _ in range(len_)] for _ in range(len_)]
+
+        index = 0
+        max_len = 1
+
+        # 可以省略，后面蕴含了这一步
+        for i in range(len_):
+            dp[i][i] = True
+
+        for j in range(1, len_):
+            for i in range(j):
+                if s[i] == s[j]:
+                    if j <= i + 2:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = False
+
+                if dp[i][j]:
+                    if j - i + 1 > max_len:
+                        max_len = j - i + 1
+                        index = i
+
+        return s[index:index + max_len]
+
+
+Solution().longestPalindrome("cbbd")
