a9fd1841228a18dc2b105cec3b5113c5d6b7e20c
xiezhang <438581918@qq.com>
2020-07-30 13:23:11 +0800

    0730新增剑指offer46、64

---
+++ 剑指offer/剑指 Offer 64. 求1+2+…+n.py
@@ -0,0 +1,19 @@
+"""
+求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+示例 1：
+输入: n = 3
+输出: 6
+
+示例 2：
+输入: n = 9
+输出: 45
+"""
+
+
+class Solution:
+    def sumNums(self, n: int) -> int:
+        if n == 1:
+            return 1
+
+        return n + self.sumNums(n - 1)
