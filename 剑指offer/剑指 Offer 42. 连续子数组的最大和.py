1a7666b6430a5a2499ee30ff85f801c31a3c2240
xiezhang <438581918@qq.com>
2020-08-05 11:39:33 +0800

    0805新增剑指offer56、27、14、10、42

---
+++ 剑指offer/剑指 Offer 42. 连续子数组的最大和.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Time   : 2020/8/5 11:21
+# Author : Zhang Xie
+
+"""
+输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+要求时间复杂度为O(n)。
+
+
+示例1:
+输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ 
+
+提示：
+1 <= arr.length <= 10^5
+-100 <= arr[i] <= 100
+"""
+
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        len_ = len(nums)
+        dp = [0] * len_
+        dp[0] = nums[0]
+        for i in range(1, len_):
+            if dp[i - 1] > 0:
+                dp[i] = dp[i - 1] + nums[i]
+            else:
+                dp[i] = nums[i]
+
+        return max(dp)
+
+
+Solution().maxSubArray([-2, -1, 2, -3])
